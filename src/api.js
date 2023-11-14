dd43cc5166d3f3287130e0f568177d19ed4a8f93
kanika-singh-28 <muskan.ms.singh4@gmail.com>
2023-11-14 16:44:27 +0530

    first commit

---
+++ src/api.js
@@ -0,0 +1,11 @@
+export const geoApiOptions = {
+    method: "GET",
+    headers: {
+      "X-RapidAPI-Key": "3fccb452b2msh0099fd7c34f9cbap16bd38jsnd09a80ff0b68",
+      "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+    },
+  };
+  export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+  
+  export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+  export const WEATHER_API_KEY = "6fd34cf1f1f726bc846d5b0f8dbb5233"; 
